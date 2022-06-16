3fdd4f15eac865909a63599bbe1a680214b7a73c
carlosaacf <carlosaacf@gmail.com>
2022-06-15 22:56:28 -0300

    inclusao de switch

---
+++ IdeaProjects/Contextualizada/src/ApiContent.java
@@ -0,0 +1,17 @@
+public class ApiContent {
+
+    public static void main(String[] args) {
+
+        int a = 0;
+        while (a < 10000) {
+
+            System.out.println(a);
+            a++;
+        }
+        while (a >= 5000) {
+
+            System.out.println(a);
+            a--;
+        }
+    }
+}
